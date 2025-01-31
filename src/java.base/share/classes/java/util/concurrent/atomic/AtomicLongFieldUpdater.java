88c7942b393561b3d7fbd74a3d80633206205f8d
altrisi <altrisi.trillosierra@gmail.com>
2025-01-31 18:59:21 +0100

    Use in AtomicLongFieldUpdater

--- src/java.base/share/classes/java/util/concurrent/atomic/AtomicLongFieldUpdater.java
+++ src/java.base/share/classes/java/util/concurrent/atomic/AtomicLongFieldUpdater.java
@@ -406,7 +406,7 @@ public abstract class AtomicLongFieldUpdater<T> {
             // narrowed to the type of the accessing class.
             this.cclass = (Modifier.isProtected(modifiers) &&
                            tclass.isAssignableFrom(caller) &&
-                           !isSamePackage(tclass, caller))
+                           !Reflection.inSamePackage(tclass, caller))
                           ? caller : tclass;
             this.tclass = tclass;
             this.offset = U.objectFieldOffset(field);
@@ -509,14 +509,5 @@ public abstract class AtomicLongFieldUpdater<T> {
             }
         } while (acl != null);
         return false;
-    }
-
-    /**
-     * Returns true if the two classes have the same class loader and
-     * package qualifier
-     */
-    static boolean isSamePackage(Class<?> class1, Class<?> class2) {
-        return class1.getClassLoader() == class2.getClassLoader()
-               && class1.getPackageName() == class2.getPackageName();
     }
 }
