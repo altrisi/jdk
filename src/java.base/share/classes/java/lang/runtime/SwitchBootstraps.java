893d198f1ce762f0a12481cf56ce1b7caf960864
altrisi <altrisi.trillosierra@gmail.com>
2025-02-07 19:09:49 +0100

    Fix import

--- src/java.base/share/classes/java/lang/runtime/SwitchBootstraps.java
+++ src/java.base/share/classes/java/lang/runtime/SwitchBootstraps.java
@@ -35,7 +35,7 @@ import java.lang.invoke.ConstantCallSite;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
-import java.lang.reflect.AccessFlag;
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
