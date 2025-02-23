78ad9e6c034ecaf0dfccb5dbea0a9ee84825fcea
altrisi <altrisi.trillosierra@gmail.com>
2025-02-23 19:16:25 +0100

    Update ModuleSetAccessibleTest

--- test/jdk/java/lang/reflect/AccessibleObject/ModuleSetAccessibleTest.java
+++ test/jdk/java/lang/reflect/AccessibleObject/ModuleSetAccessibleTest.java
@@ -148,7 +148,7 @@ public class ModuleSetAccessibleTest {
 
         // non-public constructor
         Constructor<?> ctor
-            = Class.class.getDeclaredConstructor(ClassLoader.class, Class.class, int.class, ProtectionDomain.class);
+            = Class.class.getDeclaredConstructor(ClassLoader.class, Module.class, Class.class, int.class, ProtectionDomain.class, Object.class);
         AccessibleObject[] ctors = { ctor };
 
         try {
